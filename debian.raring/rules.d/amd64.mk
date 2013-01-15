948a8d0cb639ed302d27260ded3e451292350a2d
Tim Gardner <tim.gardner@canonical.com>
2013-01-15 05:52:59 -0700

    UBUNTU: [Config] Build binary dependent tools package
    BugLink: http://bugs.launchpad.net/bugs/1088454
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian.raring/rules.d/amd64.mk
+++ debian.raring/rules.d/amd64.mk
@@ -13,6 +13,6 @@ uefi_signed     = true
 skipabi         = true
 skipmodule      = true
 do_libc_dev_package=false
-do_tools        = false
+do_tools        = true
 do_doc_package  = false
 do_source_package=false
